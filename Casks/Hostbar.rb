fb62cf7ad570dc041faac0eb2ccbc608a08d96d1
joododok <shield41791@gmail.com>
2026-04-18 00:55:34 +0900

    add Hostbar cask

---
+++ Casks/Hostbar.rb
@@ -0,0 +1,11 @@
+cask "Hostbar" do
+  version "1.0.0"
+  sha256 "b6c1f04d018bd3ff9088519e93a6031cc602cbac067f688aa35c1b8bcfa6eaeb"
+
+  url "https://github.com/shield41791/hostbar/releases/download/1.0.0/Hostbar.zip"
+  name "Hostbar"
+  desc "Hosts file manager"
+  homepage "https://github.com/shield41791/hostbar"
+
+  app "Hostbar.app"
+end
