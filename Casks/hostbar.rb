21d6341ba26c40d360151ecfaead9942439dd9e4
joododok <shield41791@gmail.com>
2026-04-18 10:36:40 +0900

    fix hostbar.rb (version)

--- Casks/hostbar.rb
+++ Casks/hostbar.rb
@@ -2,7 +2,7 @@ cask "hostbar" do
   version "1.0.1"
   sha256 "ee47831e7ad5fa438d124a0d271c8e93c6001ee88d226ad0cd950049812f738a"
 
-  url "https://github.com/shield41791/hostbar/releases/download/v1.0.1/Hostbar.zip"
+  url "https://github.com/shield41791/hostbar/releases/download/v#{version}/Hostbar.zip"
   name "Hostbar"
   desc "Hosts file manager"
   homepage "https://github.com/shield41791/hostbar"
